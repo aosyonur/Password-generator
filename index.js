2e2e787ee4a368189afd60bc664c4cbdbf782d76
aosyonur <104443985+aosyonur@users.noreply.github.com>
2022-12-13 01:30:07 +0300

    Password generator files

---
+++ index.js
@@ -0,0 +1,131 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+let passwordLength = 12;
+
+function getRandomCharacter() {
+  let randomCharacter = Math.floor(Math.random() * characters.length);
+  return characters[randomCharacter];
+}
+
+function generate() {
+  let randomPasswordOne = "";
+  let randomPasswordTwo = "";
+  for (let index = 0; index < passwordLength; index++) {
+    randomPasswordOne += getRandomCharacter();
+    randomPasswordTwo += getRandomCharacter();
+  }
+  passwordOne.textContent = randomPasswordOne;
+  passwordTwo.textContent = randomPasswordTwo;
+}
+
+// IT WORKS LIKE THIS TOO WITHOUT "getRandomCharacter function" //
+
+// function generate() {
+//   let randomPasswordOne = "";
+//   let randomPasswordTwo = "";
+//   for (let i = 0; i < passwordLength; i++) {
+//     let randomCharacter = Math.floor(Math.random() * characters.length);
+//     randomPasswordOne += characters[randomCharacter];
+//     let randomCharacter2 = Math.floor(Math.random() * characters.length);
+//     randomPasswordTwo += characters[randomCharacter2];
+//   }
+//   passwordOne.textContent = randomPasswordOne;
+//   passwordTwo.textContent = randomPasswordTwo;
+// }
+
+// IT WORKS LIKE THIS TOO //
